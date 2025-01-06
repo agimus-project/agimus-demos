5685a51b986d67f9c9eecc637265f2cd20850e33
Théo Martinez <tmartinezf@laas.fr>
2025-01-06 10:05:05 +0000

    move the parameters in mpc repo since we use the generate_parameter_library

--- agimus_demo_03_mpc_dummy_traj/launch/bringup.launch.py
+++ agimus_demo_03_mpc_dummy_traj/launch/bringup.launch.py
@@ -184,15 +184,12 @@ def prepare_launch_description():
     )
 
     # MPC
-    mpc_params_yaml = str(
-        Path(get_package_share_directory(package_name)) / "config" / "mpc_params.yaml"
-    )
     mpc_controller = Node(
         package="agimus_controller_ros",
         executable="agimus_controller_node",
         name="agimus_controller_node",
         output="screen",
-        parameters=[mpc_params_yaml],
+        parameters=[],
     )
 
     return LaunchDescription(
