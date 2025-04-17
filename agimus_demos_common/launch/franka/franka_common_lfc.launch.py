cdb3da6aa5d8896f1751ed89702d998d5a95fb4c
Maximilien Naveau <maximilien.naveau@gmail.com>
2025-04-17 10:20:53 +0200

    Restor and update the launch files.

--- agimus_demos_common/launch/franka/franka_common_lfc.launch.py
+++ agimus_demos_common/launch/franka/franka_common_lfc.launch.py
@@ -1,9 +1,11 @@
 from launch import LaunchContext, LaunchDescription
 from launch.actions import (
     DeclareLaunchArgument,
+    IncludeLaunchDescription,
     OpaqueFunction,
 )
 from launch.launch_description_entity import LaunchDescriptionEntity
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import (
     LaunchConfiguration,
     PathJoinSubstitution,
@@ -12,7 +14,6 @@ from launch_ros.substitutions import FindPackageShare
 
 from agimus_demos_common.launch_utils import (
     generate_default_franka_args,
-    generate_include_launch,
 )
 
 
@@ -23,15 +24,41 @@ def launch_setup(
         "linear_feedback_controller",
         "joint_state_estimator",
     ]
-    franka_robot_launch = generate_include_launch(
-        "franka_common.launch.py",
-        extra_launch_arguments={
-            "external_controllers_names": str(linear_feedback_controllers_names),
+
+    franka_robot_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare("agimus_demos_common"),
+                        "launch",
+                        "franka",
+                        "franka_common.launch.py",
+                    ]
+                )
+            ]
+        ),
+        launch_arguments={
+            "arm_id": LaunchConfiguration("arm_id"),
+            "aux_computer_ip": LaunchConfiguration("aux_computer_ip"),
+            "aux_computer_user": LaunchConfiguration("aux_computer_user"),
+            "on_aux_computer": LaunchConfiguration("on_aux_computer"),
+            "robot_ip": LaunchConfiguration("robot_ip"),
             "external_controllers_params": LaunchConfiguration(
                 "linear_feedback_controller_params"
             ),
-        },
+            "external_controllers_names": str(linear_feedback_controllers_names),
+            "franka_controllers_params": LaunchConfiguration(
+                "franka_controllers_params"
+            ),
+            "use_gazebo": LaunchConfiguration("use_gazebo"),
+            "use_rviz": LaunchConfiguration("use_rviz"),
+            "rviz_config_path": LaunchConfiguration("rviz_config_path"),
+            "gz_verbose": LaunchConfiguration("gz_verbose"),
+            "gz_headless": LaunchConfiguration("gz_headless"),
+        }.items(),
     )
+
     return [franka_robot_launch]
 
 
@@ -44,7 +71,7 @@ def generate_launch_description():
                     FindPackageShare("agimus_demos_common"),
                     "config",
                     "franka",
-                    "franka_controllers.yaml",
+                    "controllers.yaml",
                 ]
             ),
             description="Path to the yaml file use to define controller parameters.",
@@ -55,6 +82,7 @@ def generate_launch_description():
                 [
                     FindPackageShare("agimus_demos_common"),
                     "config",
+                    "franka",
                     "linear_feedback_controller_params.yaml",
                 ]
             ),
