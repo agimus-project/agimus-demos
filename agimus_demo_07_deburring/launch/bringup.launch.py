f857808b3f4f5861a2c3a6f0dfbd5aa3fbba8c7f
Krzysztof Wojciechowski <krzy.wojciecho@gmail.com>
2025-09-29 19:12:45 +0000

    Port panda_deburring

--- agimus_demo_07_deburring/launch/bringup.launch.py
+++ agimus_demo_07_deburring/launch/bringup.launch.py
@@ -1,41 +1,53 @@
 from agimus_demos_common.launch_utils import (
     generate_default_franka_args,
-    get_use_sim_time,
-    ament_prefix_to_ros_package,
+    generate_include_launch,
 )
+from agimus_demos_common.static_transform_publisher_node import (
+    static_transform_publisher_node,
+)
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
-from launch_ros.actions import Node
 from launch import LaunchContext, LaunchDescription
-from launch.actions import OpaqueFunction, ExecuteProcess
+from launch.actions import OpaqueFunction
 from launch.launch_description_entity import LaunchDescriptionEntity
-from launch.substitutions import PathJoinSubstitution, Command, FindExecutable
-from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 
 
 def launch_setup(
     context: LaunchContext, *args, **kwargs
 ) -> list[LaunchDescriptionEntity]:
-    # pytroller_params = (
-    #     PathJoinSubstitution(
-    #         [
-    #             FindPackageShare("agimus_demo_07_deburring"),
-    #             "config",
-    #             "pytroller_params.yaml",
-    #         ]
-    #     ),
-    # )
+    rviz_config_path = PathJoinSubstitution(
+        [
+            FindPackageShare("agimus_demo_07_deburring"),
+            "rviz",
+            "config.rviz",
+        ]
+    )
 
-    # agimus_pytroller_names = ["agimus_pytroller", "ft_calibration_filter"]
+    pytroller_params = (
+        PathJoinSubstitution(
+            [
+                FindPackageShare("agimus_demo_07_deburring"),
+                "config",
+                "pytroller_params.yaml",
+            ]
+        ),
+    )
 
-    # franka_robot_launch = generate_include_launch(
-    #     "franka_common.launch.py",
-    #     extra_launch_arguments={
-    #         "external_controllers_names": str(agimus_pytroller_names),
-    #         "external_controllers_params": pytroller_params,
-    #         "use_ft_sensor": "true",
-    #     },
-    # )
+    agimus_pytroller_names = ["agimus_pytroller", "ft_calibration_filter"]
+
+    franka_robot_launch = generate_include_launch(
+        "franka_common.launch.py",
+        extra_launch_arguments={
+            "external_controllers_names": str(agimus_pytroller_names),
+            "external_controllers_params": pytroller_params,
+            "rviz_config_path": rviz_config_path,
+            "use_ft_sensor": "true",
+            "ee_id": "ati_mini45_with_camera",
+        },
+    )
 
     environment_description = ParameterValue(
         Command(
@@ -49,6 +61,8 @@ def launch_setup(
                         "environment.urdf.xacro",
                     ]
                 ),
+                " ",
+                "with_sc:=true",
             ]
         ),
         value_type=str,
@@ -59,50 +73,14 @@ def launch_setup(
         name="environment_publisher",
         output="screen",
         remappings=[("robot_description", "environment_description")],
-        parameters=[get_use_sim_time(), {"robot_description": environment_description}],
+        parameters=[{"robot_description": environment_description}],
+    )
+    tf_node = static_transform_publisher_node(
+        frame_id="fer_link0",
+        child_frame_id="env_ref_frame",
     )
 
-    hpp_corba_server = ExecuteProcess(
-        cmd=["hppcorbaserver"],
-        # Extra env variable for HPP to discover packages correctly
-        additional_env=ament_prefix_to_ros_package(context),
-        output="screen",
-    )
-
-    orchestrator_params = (
-        PathJoinSubstitution(
-            [
-                FindPackageShare("agimus_demo_07_deburring"),
-                "config",
-                "orchestrator_params.yaml",
-            ]
-        ),
-    )
-
-    deburring_orchestrator_node = Node(
-        prefix="xterm -hold -e",
-        package="agimus_demo_07_deburring",
-        executable="deburring_orchestrator_node",
-        parameters=[get_use_sim_time(), orchestrator_params],
-        # Extra env variable for HPP to discover packages correctly
-        additional_env=ament_prefix_to_ros_package(context),
-        output="screen",
-    )
-
-    # panda_to_pylone_tf = static_transform_publisher_node(
-    #     frame_id="fer_link0",
-    #     child_frame_id="pylone_link",
-    #     xyz=(0.5, 0.0, 0.0),
-    #     rot_rpy=(0.0, 0.0, 0.0),
-    # )
-
-    return [
-        # franka_robot_launch,
-        hpp_corba_server,
-        deburring_orchestrator_node,
-        # panda_to_pylone_tf,
-        environment_publisher_node,
-    ]
+    return [franka_robot_launch, environment_publisher_node, tf_node]
 
 
 def generate_launch_description():
