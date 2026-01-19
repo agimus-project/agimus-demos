aad71b635f333e96ca98dfcddbeea0490434ecaa
Krzysztof Wojciechowski <krzy.wojciecho@gmail.com>
2026-01-19 10:58:17 +0000

    Add syncronous capabilities to the sequencer node

--- agimus_demo_07_deburring/agimus_demo_07_deburring/sequencer_node.py
+++ agimus_demo_07_deburring/agimus_demo_07_deburring/sequencer_node.py
@@ -6,10 +6,10 @@ from rclpy.node import Node
 from rclpy.action import ActionClient
 from rclpy.exceptions import ParameterException
 from rclpy.parameter import Parameter
-from rclpy.task import Future
 
 from rcl_interfaces.srv import SetParametersAtomically
 
+from action_msgs.msg import GoalStatus
 from agimus_msgs.action import DeburringPlanner
 
 
@@ -18,7 +18,7 @@ class SequencerNode(Node):
         super().__init__("sequencer_node")
 
         self._deburring_action_client = ActionClient(
-            self, DeburringPlanner, "/plan_deburring"
+            self, DeburringPlanner, "/deburring_path_planner_node/plan_deburring"
         )
 
         self._set_param_cli = self.create_client(
@@ -28,34 +28,58 @@ class SequencerNode(Node):
 
         self.get_logger().info("Node started.")
 
-    def _set_params(self, parameters: list[Parameter], timeout: float = 5.0) -> None:
+    def set_weights_value(self, value: list[float], timeout: float = 5.0) -> None:
+        """Sets parameter `weights.deburring_motion.w_desired_force` of external
+        node.
+
+        Args:
+            value (list[float]): 3 element list containing weights to be set for deburring force.
+            timeout (float, optional): Time to wait for the service to be available. Defaults to 5.0.
+
+        Raises:
+            RuntimeError: Exception thrown in case the node is unreachable.
+        """
         start = time.time()
         ready = False
         while time.time() - start < timeout and not ready:
             ready = self._set_param_cli.service_is_ready()
+            if not ready:
+                self.get_logger().debug(
+                    f"Waiting for service {self._set_param_cli.srv_name}...",
+                    throttle_duration_sec=1.0,
+                )
             time.sleep(0.1)
 
         if not ready:
-            raise RuntimeError(f"Service {self._set_param_cli.srv_name} is not ready!")
+            raise RuntimeError(
+                f"Service {self._set_param_cli.srv_name} is not ready!",
+            )
 
         req = SetParametersAtomically.Request(
-            parameters=[param.to_parameter_msg() for param in parameters]
-        )
-        future = self._set_param_cli.call_async(req)
-        rclpy.spin_until_future_complete(self, future)
-
-    def set_weights_value(self, value: list[float]) -> None:
-        self._set_params(
-            [
+            parameters=[
                 Parameter(
                     "weights.deburring_motion.w_desired_force",
                     Parameter.Type.DOUBLE_ARRAY,
                     value,
-                )
+                ).to_parameter_msg()
             ]
         )
+        future = self._set_param_cli.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
 
-    def plan_to_handle(self, handle: str, do_insertion: bool) -> Future:
+    def plan_to_handle(self, handle: str, do_insertion: bool) -> None:
+        """_summary_
+
+        Args:
+            handle (str): Name of the handle to which robot has to move.
+            do_insertion (bool): Whether to perform insertion and deburring motion
+                during asked movement.
+
+        Raises:
+            RuntimeError: Failed to reach the action server.
+            RuntimeError: Action was rejected.
+            RuntimeError: Action execution failed.
+        """
         goal_msg = DeburringPlanner.Goal(
             handle_name=handle,
             do_insertion=do_insertion,
@@ -65,21 +89,30 @@ class SequencerNode(Node):
         future = self._deburring_action_client.send_goal_async(
             goal_msg, feedback_callback=self._feedback_callback
         )
-        future.add_done_callback(self._goal_response_callback)
 
-        return future
+        rclpy.spin_until_future_complete(self, future, timeout_sec=5.0)
+        if not future.done():
+            raise RuntimeError(
+                f"Action {self._deburring_action_client.action_name} does not respond!",
+            )
 
-    def _goal_response_callback(self, future: Future) -> None:
         goal_handle = future.result()
         if not goal_handle.accepted:
-            self.get_logger().warn(
-                f"Target goal rejected. Reason: '{goal_handle.error_msgs}'"
-            )
-            return
+            raise RuntimeError("Action call was rejected!")
 
-        self.get_logger().info("Target goal accepted.")
+        self.get_logger().debug("Target goal accepted.")
 
-        self._get_result_future = goal_handle.get_result_async()
+        future = goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(self, future)
+
+        if future.result().status != GoalStatus.STATUS_SUCCEEDED:
+            msg = "Action failed!"
+            error_msg = future.result().result.error_msg
+            if error_msg != "":
+                msg += f" Error message: '{error_msg}'."
+            raise RuntimeError(msg)
+
+        self.get_logger().debug("Action succeed.")
 
     def _feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
@@ -99,7 +132,11 @@ def main(args=None) -> int:
         print("  example: ros2 run agimus_demo_07_deburring sequencer_node metal")
         return 0
 
-    if len(args) != 2 or len({"metal", "plastic"} & set(args)) == 0:
+    if (
+        "--ros-args" not in args
+        and len(args) != 2
+        or len({"metal", "plastic"} & set(args)) == 0
+    ):
         print("Incorrect parameters! Use -h or --help for more information.")
         return 1
 
@@ -134,12 +171,13 @@ def main(args=None) -> int:
                 == "n"
             ):
                 continue
-            sequencer_node.set_weights_value(weight)
-            future = sequencer_node.plan_to_handle(handle_name, False)
-            rclpy.spin_until_future_complete(sequencer_node, future)
-            input("Please refine the pose")
-            future = sequencer_node.plan_to_handle(handle_name, True)
-            rclpy.spin_until_future_complete(sequencer_node, future)
+            try:
+                # sequencer_node.set_weights_value(weight)
+                sequencer_node.plan_to_handle(handle_name, False)
+                input("Please refine the pose")
+                sequencer_node.plan_to_handle(handle_name, True)
+            except RuntimeError as err:
+                print(f"Error. {str(err)}. Skipping the handle!")
 
         print("Sequencing done, stopping the node")
 
