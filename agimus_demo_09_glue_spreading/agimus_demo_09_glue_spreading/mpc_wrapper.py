f021c28410b65c13490384e0867d648b5da44268
LouiseMsn <louise.mansn@gmail.com>
2026-01-23 12:52:13 +0000

    refacto params + debug publishers

--- agimus_demo_09_glue_spreading/agimus_demo_09_glue_spreading/mpc_wrapper.py
+++ agimus_demo_09_glue_spreading/agimus_demo_09_glue_spreading/mpc_wrapper.py
@@ -58,19 +58,19 @@ class AligatorMPC(Node):
         config_path = self.get_parameter('config').value
 
         self.get_logger().info(config_path)
-        self.mpc_parameters = Params(Path(config_path))
+        self.mpc_parameters = Config.from_yaml(Path(config_path))
 
         # Waypoints ================================================================
         patternGen = PatternGenerator([0.3,0.3,0], (0.5, 0,0.1))
         mpc_waypoints = patternGen.generate_pattern('zigzag_curve',stride=0.05)
         
-        test_trajs = TestTrajs()
+        # test_trajs = TestTrajs()
         # start = pin.SE3(pin.rpy.rpyToMatrix(np.pi,0,0), np.array([0.5, -0.2, 0.2]))
         # end =  pin.SE3(pin.rpy.rpyToMatrix(np.pi,0,np.pi/2), np.array([0.5, 0.2, 0.2]))
         # mpc_waypoints = [start, end]
 
-        startsin = [0.3, -0., 0.2]
-        mpc_waypoints = test_trajs.sine(start_point=startsin,length=0.3,period=0.03,amplitude=0.15, dist_between_points=0.01, sine_axis="Y", ampl_axis="X")
+        # startsin = [0.3, -0., 0.2]
+        # mpc_waypoints = test_trajs.sine(start_point=startsin,length=0.3,period=0.03,amplitude=0.15, dist_between_points=0.01, sine_axis="Y", ampl_axis="X")
 
         self.waypoints_marker_msg = Marker() #self.waypoints_to_marker(mpc_waypoints)
         
@@ -78,7 +78,7 @@ class AligatorMPC(Node):
         self.robot_state = None
         self.mpc = MPC(mpc_waypoints, self.mpc_parameters)
         self.first_mpc_iteration = True
-        self.feedback_gain = 1e-2
+        self.feedback_gain = 1e-4
 
         # ROS2 publishers & subscribers ============================================
         self.control_publisher = self.create_publisher(Control, "control", 10)
